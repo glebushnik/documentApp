eef08ad0aa0943169f8e27e6647eea1308166755
glebushnik <glebushnik@MacBook-Pro-glebushnik.local>
2024-05-15 18:05:10 +0300

    log

--- src/main/java/com/api/documentApp/security/SecurityConfiguration.java
+++ src/main/java/com/api/documentApp/security/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration {
                     return corsConfiguration;
                 }))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**" , "/v3/api-docs/**", "/api/auth/**", "/api/docs/{docId}").permitAll()
+                        .requestMatchers("/swagger-ui/**" , "/v3/api-docs/**", "/api/auth/**", "/api/docs/{docId}", "/log").permitAll()
                         .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
